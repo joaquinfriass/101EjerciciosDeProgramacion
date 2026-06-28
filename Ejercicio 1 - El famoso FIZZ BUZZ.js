647b0bcf6a5e0bb33aeaed137267dd72d58a13c4
Joaquin Frias <joaquin124507@gmail.com>
2026-06-28 11:04:48 -0300

    Desarrollo Ejercicio FizzBuzz

--- Ejercicio 1 - El famoso FIZZ BUZZ.js
+++ Ejercicio 1 - El famoso FIZZ BUZZ.js
@@ -9,4 +9,26 @@
  */
 
 
-// function fizzBuzz()
+function fizzBuzz(){
+    // Iteramos del 1 al 100
+    for (let i = 1; i <= 100; i++) {
+        // Comprobamos si el número es múltiplo de 3 y de 5
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz");
+        }
+        // Comprobamos si el número es múltiplo de 3
+        else if (i % 3 === 0) {
+            console.log("fizz");
+        }
+        // Comprobamos si el número es múltiplo de 5
+        else if (i % 5 === 0) {
+            console.log("buzz");
+        }
+        // Si no es múltiplo de 3 ni de 5, imprimimos el número
+        else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz();
